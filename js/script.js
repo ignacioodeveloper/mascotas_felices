63d5601c0c51eb98248692ad9c1c98767fbf443e
luis <luisignaciocontacto@gmail.com>
2023-05-23 14:55:45 -0400

    ropa api conectada test

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
 $(document).ready(function(){
 
+  $('#btn-obtener-ropas').click(function() {
+    
+    // funcion obtener datos api fakestore
+    $.get('https://fakestoreapi.com/products',
+    
+    function(datos) 
+    {   
+        $('#tabla-ropas tbody').empty();
+        $.each(datos, function(i, item) {
+            
+            var fila = '';
+            fila += '<tr>' ;
+            fila += '<td>' + item.title + '</td>';
+            fila += '<td>' + item.description + '</td>';
+            fila += '<td>' + item.price + '</td>';
+            fila += '<td><img src="'+ item.image +'"></td>';
+
+            fila += '</tr>';
+
+            $('#tabla-ropas').append(fila);
+        });
+    });
+  });
+
+  // funcion carrusel
   $('.slick-carousel').slick({
     // opciones de configuración de Slick
     dots: true,
@@ -65,3 +90,5 @@ document.addEventListener("DOMContentLoaded", function() {
   loadHTMLContent("navbar.html", navbarContainer);
   loadHTMLContent("footer.html", footerContainer);
 });
+
+
