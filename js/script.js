7d058561fd54b091abd9cacf9db9108a9051b7ff
Luis Barboza <lu.barboza@duocuc.cl>
2023-05-25 00:10:51 -0300

    añadiendo funciones y validaciones v1

--- js/script.js
+++ js/script.js
@@ -83,42 +83,42 @@ $(document).ready(function(){
   });
 
 
-
   // Agregar método de validación para RUT chileno
   $.validator.addMethod("rutChileno", function(value, element) {
-      // Eliminar puntos y guión del RUT
-      value = value.replace(/[.-]/g, "");
+    // Eliminar puntos y guión del RUT
+    value = value.replace(/[.-]/g, "");
   
-      // Validar que el RUT tenga 8 o 9 dígitos
-      if (value.length < 8 || value.length > 9) {
-        return false;
-      }
+    // Validar que el RUT tenga 8 o 9 dígitos
+    if (value.length < 8 || value.length > 9) {
+      return false;
+    }
   
-      // Validar que el último dígito sea un número o una 'K'
-      var validChars = "0123456789K";
-      var lastChar = value.charAt(value.length - 1).toUpperCase();
-      if (validChars.indexOf(lastChar) == -1) {
-        return false;
-      }
+    // Validar que el último dígito sea un número o una 'K'
+    var validChars = "0123456789K";
+    var lastChar = value.charAt(value.length - 1).toUpperCase();
+    if (validChars.indexOf(lastChar) == -1) {
+      return false;
+    }
   
-      // Calcular el dígito verificador
-      var rut = parseInt(value.slice(0, -1), 10);
-      var factor = 2;
-      var sum = 0;
-      var digit;
-      while (rut > 0) {
-        digit = rut % 10;
-        sum += digit * factor;
-        rut = Math.floor(rut / 10);
-        factor = factor === 7 ? 2 : factor + 1;
-      }
-      var dv = 11 - (sum % 11);
-      dv = dv === 11 ? "0" : dv === 10 ? "K" : dv.toString();
+    // Calcular el dígito verificador
+    var rut = parseInt(value.slice(0, -1), 10);
+    var factor = 2;
+    var sum = 0;
+    var digit;
+    while (rut > 0) {
+      digit = rut % 10;
+      sum += digit * factor;
+      rut = Math.floor(rut / 10);
+      factor = factor === 7 ? 2 : factor + 1;
+    }
+    var dv = 11 - (sum % 11);
+    dv = dv === 11 ? "0" : dv === 10 ? "K" : dv.toString();
   
-      // Validar que el dígito verificador sea correcto
-      return dv === lastChar;
-    }, "Por favor ingrese un RUT válido."); 
+    // Validar que el dígito verificador sea correcto
+    return dv === lastChar;
+  }, "Por favor ingrese un RUT válido."); 
 
+  
   $("#formulario1").validate({
     rules: {
       rut: {
@@ -138,6 +138,7 @@ $(document).ready(function(){
         equalTo: "#password",
       },
     }, // --> Fin de reglas
+
     messages: {
       rut: {
         required: "El rut es un campo obligatorio",
@@ -158,8 +159,37 @@ $(document).ready(function(){
     },
   });
 
+
+
+});
+
+
+
+// script titulo typing efect
+document.addEventListener('DOMContentLoaded',function(event){
+
+function generarCodigoAleatorio() {
+  var codigo = Math.floor(Math.random() * 10000) + 1;
+  return codigo;
+}
+
+const copyEmailBtn = document.querySelector('#copy-email');
+
+copyEmailBtn.addEventListener('click', () => {
+const email = generarCodigoAleatorio();
+
+// Copiar el email al portapapeles
+navigator.clipboard.writeText(email)
+  .then(() => {
+    // Cambiar el texto del botón
+    copyEmailBtn.innerText = 'Codigo de Descuento Copiado!';
+  })
+  .catch(err => {
+    console.error('No se pudo copiar el Codigo de Descuento: ', err);
+  });
 });
 
 
 
 
+});
